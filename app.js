0a4f0c061eeb09603184b285d9cb3589e0f8c774
Carlton Joseph <carlton.joseph@gmail.com>
2020-08-15 09:51:26 -0700

    add live reload

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use("/static", express.static("public"));
+
 app.get("/", (req, res) => {
   res.send(`Saw request at ${req.url}.`);
 });
