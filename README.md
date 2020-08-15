00de8bb68df2fbd3691d8301be19e0cb0ff9ab9e
Carlton Joseph <carlton.joseph@gmail.com>
2020-08-15 09:08:42 -0700

    initial commit - express ejs

---
+++ README.md
@@ -0,0 +1,5 @@
+# Express Auth with MongoDb
+
+The code in this repository is based on
+[Node Auth Tutorial (JWT)](https://youtu.be/SnoAwLP1a-0)
+video.
---
+++ app.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send(`Saw request at ${req.url}.`);
+});
+
+app.get("/index", (req, res) => {
+  res.render("pages/index");
+});
+
+module.exports = app;
